f31e030e1d87c6563ae79552e68cb0bd45cb7b1a
rmzNadir <dgzrz99@gmail.com>
2021-02-01 16:19:58 -0600

    articles controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+  resources :articles
+  # get '/articles'
+  # post '/articles'
+  # delete '/articles'
+  # get '/articles/new'
+  # get '/articles/:id'
+  # get '/articles/:id/edit'
+  # patch '/articles/:id'
+  # put '/articles/:id'
   root 'welcome#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
