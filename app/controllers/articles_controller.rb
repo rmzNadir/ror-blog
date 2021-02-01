0322ce1e35b9a72e64657f2cac724a5ff7c65aea
rmzNadir <dgzrz99@gmail.com>
2021-02-01 16:51:15 -0600

    :sparkles: create articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,4 +8,16 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  # GET /articles/new
+  def new
+    @article = Article.new
+  end
+
+  # POST /articles
+  def create
+    @article = Article.new(title: params[:article][:title], body: params[:article][:body])
+    @article.save
+    redirect_to @article
+  end
 end
