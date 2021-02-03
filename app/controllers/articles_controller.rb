d403dd41ccdefc13515978bb79f1ff80b80ab516
rmzNadir <dgzrz99@gmail.com>
2021-02-03 15:05:06 -0600

    :construction: strong params

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,7 +16,7 @@ class ArticlesController < ApplicationController
 
   # POST /articles
   def create
-    @article = Article.new(title: params[:article][:title], body: params[:article][:body])
+    @article = Article.new(article_params)
     if @article.save
       redirect_to @article
     else
@@ -33,5 +33,11 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
     @article.destroy
     redirect_to articles_path
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :body)
   end
 end
